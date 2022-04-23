4ffccba7810247e5b78e1f81e652f163c0b91d73
Seyi Egunjobi <seyistry@gmail.com>
2022-04-23 21:00:23 +0100

    add theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 import Home from "./screen/Home";
 import Dashboard from "./screen/Dashboard";
 
+const theme = createTheme({
+    typography: {
+        fontFamily: [
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+        ].join(","),
+    },
+});
+
 function App() {
     const [upload, setUpload] = useState("");
     const [fileTitle, setFileTitle] = useState("");
-    return upload ? (
-        <Dashboard
-            setUpload={setUpload}
-            upload={upload}
-            fileTitle={fileTitle}
-        />
-    ) : (
-        <Home
-            setUpload={setUpload}
-            upload={upload}
-            setFileTitle={setFileTitle}
-        />
+    return (
+        <ThemeProvider theme={theme}>
+            {upload ? (
+                <Dashboard
+                    setUpload={setUpload}
+                    upload={upload}
+                    fileTitle={fileTitle}
+                />
+            ) : (
+                <Home
+                    setUpload={setUpload}
+                    upload={upload}
+                    setFileTitle={setFileTitle}
+                />
+            )}
+        </ThemeProvider>
     );
 }
 

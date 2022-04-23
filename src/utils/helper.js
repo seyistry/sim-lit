cf718eeda4a776840e6a5e25ca0ed326f3277eca
Seyi Egunjobi <seyistry@gmail.com>
2022-04-23 17:10:33 +0100

    add new ARTStartDateCollections function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,34 @@
 import moment from "moment";
 
+export const ARTStartDateCollections = (store) => {
+    const dateArray = [];
+    store.filter((list) => {
+        const dateCo = moment(list.ARTStartDate, "DD/MM/YYYY").format(
+            "YYYY-MM-DD"
+        );
+        let found = false;
+        for (var i = 0; i < dateArray.length; i++) {
+            if (dateArray[i].day === dateCo) {
+                found = true;
+                dateArray[i].value = dateArray[i].value + 1;
+                break;
+            }
+        }
+        if (!found) {
+            const myObj = {};
+            myObj["day"] = dateCo;
+            myObj["value"] = 1;
+            dateArray.push(myObj);
+        }
+        return null;
+    });
+    return dateArray;
+};
+
 export const activePatients = (store) => {
     const info = store.filter(
-        (list) => list.CurrentARTStatus_Pharmacy === "Active"
+        (list) =>
+            list.CurrentARTStatus_Pharmacy === "Active" && list.Outcomes === ""
     );
     return info;
 };
@@ -29,10 +55,7 @@ export const eligiblePatients = (store, date) => {
 
 export const viralLoadDocumented = (store, date) => {
     const info = store.filter((list) => {
-        const ReffDate = moment(date, "DD/MM/YYYY", true).subtract(
-            365,
-            "day"
-        );
+        const ReffDate = moment(date, "DD/MM/YYYY", true).subtract(365, "day");
         const DateofCurrentViralLoad = moment(
             list.DateofCurrentViralLoad,
             "DD/MM/YYYY",

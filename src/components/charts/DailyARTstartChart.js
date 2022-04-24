9ae3fa617dcd58090b61791fdc1bf5efa14c0425
Seyi Egunjobi <seyistry@gmail.com>
2022-04-24 21:37:40 +0100

    change color scheme

--- src/components/charts/DailyARTstartChart.js
+++ src/components/charts/DailyARTstartChart.js
@@ -1,16 +1,32 @@
+import { blue } from "@mui/material/colors";
 import { ResponsiveCalendar } from "@nivo/calendar";
 const DailyARTstartChart = (props) => {
     const data = props.artDateData;
+    const indicatorColorOne = blue[100];
+    const indicatorColorTwo = blue[300];
+    const indicatorColorThree = blue[500];
+    const indicatorColorFour = blue[900];
+
     return (
         <ResponsiveCalendar
             data={data}
             from="2021-03-01"
             to="2022-12-12"
             emptyColor="#eeeeee"
-            colors={["#abb5fa", "#4791db", "#1976D2", "#115293"]}
+            theme={{
+                // textColor: "grey",
+                // fontSize: 11,
+            }}
+            colors={[
+                indicatorColorOne,
+                indicatorColorTwo,
+                indicatorColorThree,
+                indicatorColorFour,
+            ]}
             margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
             yearSpacing={40}
             maxValue={5}
+            monthLegendPosition="after"
             monthBorderColor="#ffffff"
             dayBorderWidth={2}
             dayBorderColor="#ffffff"

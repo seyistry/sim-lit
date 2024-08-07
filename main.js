c31c8f8c1b3d9570c9372d89dc62d7e4de1f4af0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-08-07 12:08:30 +0100

    package  app with build

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require("electron");
-
-const path = require("path");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -8,11 +8,29 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
-    icon: path.join(__dirname + "/public/favicon.ico"),
+    icon: path.join(__dirname + "/public/favicon.ico"), // Ensure the icon is correctly set
   });
 
-  win.loadURL("http://localhost:3000");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, 'build', 'index.html')}`
+  );
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
